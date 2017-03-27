73397fb86321a190c871813734a610cddda83497
lsqy <lsqy93@gmail.com>
2017-03-27 23:13:31 +0800

    添加history后端配合处理

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var http = require('http');
 var express = require('express');
 var ecstatic = require('ecstatic');
+var history = require('connect-history-api-fallback');
 
 var app = express();
+
+app.use(history());
 app.use(ecstatic({ root: __dirname + '/dist' }));
+
+
 http.createServer(app).listen(7777);
 
 console.log('Listening on :7777');
